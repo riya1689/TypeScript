306170d132bde1cc4500e46a2e8c1f7942de95e3
riya1689 <riyaratri24@gmail.com>
2025-11-15 22:09:24 +0600

    polymorphism in oop

---
+++ src/object-oriented-typescript/polymorphism.ts
@@ -0,0 +1,83 @@
+// polymorphism : bohurupi
+
+//kono ekta class er method jodi different different class e vinno vinno 
+// achoron kore tahole take amra bohurupi ba polymorphism boli
+
+//same name er method differen different behave kore tokhn setio polymorphism
+
+//child theke constructor define korle super dite hoy.
+
+class Person {
+  getSleep() {
+    console.log(`I am a Normal Happy Person.I sleep for 8 hours`);
+  }
+}
+
+class Student extends Person {
+  getSleep() {
+    console.log(`I am a student. I sleep  7 hours`);
+  }
+}
+
+class NextLevelDeveloper extends Person {
+  getSleep() {
+    console.log(`I am a Next Level developer . I sleep for 6 hours`);
+  } 
+}
+
+const getSleepingHours = (param: Person) => {
+  param.getSleep();
+};
+
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new NextLevelDeveloper();
+
+getSleepingHours(person3);
+
+class Shape {
+  getArea(): number {
+    return 0;
+  }
+}
+
+class Circle extends Shape {
+  // area= pi*r*r
+  radius: number;
+
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Rectangle extends Shape {
+  // area= height*width
+
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    super();
+    this.height = height;
+    this.width = width;
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
+
+const getArea = (param: Shape) => {
+  console.log(param.getArea());
+};
+
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Rectangle(10, 20);
+
+getArea(shape3);
